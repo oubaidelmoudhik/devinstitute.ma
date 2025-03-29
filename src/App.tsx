faa990abfdf5a613e284489da14c07ac799e2bdb
oubaidelmoudhik <oubaidm@live.com>
2025-03-29 00:25:03 +0000

    started modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";  
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Wrapper from "./layouts/Wrapper";
 import HomeOne from "./components/homes/home";
 import HomeTwo from "./components/homes/home-2";
@@ -18,40 +17,34 @@ import Blog from "./components/blog";
 import BlogDetails from "./components/blog-details";
 import Contact from "./components/contact";
 import ErrorArea from "./components/error";
- 
-
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomeOne /> },
-  { path: "/home-2", element: <HomeTwo /> }, 
-  { path: "/about-us", element: <Aboutus /> }, 
-  { path: "/service", element: <Service /> }, 
-  { path: "/service-details", element: <ServiceDetails /> }, 
-  { path: "/register", element: <Register /> }, 
-  { path: "/login", element: <Login /> }, 
-  { path: "/pricing-plan", element: <PricingPlan /> }, 
-  { path: "/faq", element: <Faq /> }, 
-  { path: "/team", element: <Team /> }, 
-  { path: "/team-details", element: <TeamDetails /> }, 
-  { path: "/portfolio", element: <Portfolio /> }, 
-  { path: "/portfolio-details", element: <PortfolioDetails /> }, 
-  { path: "/blog", element: <Blog /> }, 
-  { path: "/blog-details", element: <BlogDetails /> }, 
-  { path: "/contact", element: <Contact /> }, 
-
-
+  { path: "/", element: <HomeTwo /> },
+  { path: "/home-2", element: <HomeOne /> },
+  { path: "/about-us", element: <Aboutus /> },
+  { path: "/service", element: <Service /> },
+  { path: "/service-details", element: <ServiceDetails /> },
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  { path: "/pricing-plan", element: <PricingPlan /> },
+  { path: "/faq", element: <Faq /> },
+  { path: "/team", element: <Team /> },
+  { path: "/team-details", element: <TeamDetails /> },
+  { path: "/portfolio", element: <Portfolio /> },
+  { path: "/portfolio-details", element: <PortfolioDetails /> },
+  { path: "/blog", element: <Blog /> },
+  { path: "/blog-details", element: <BlogDetails /> },
+  { path: "/contact", element: <Contact /> },
 
   { path: "*", element: <ErrorArea /> },
 ]);
 
-
 function App() {
-
   return (
-    <Wrapper> 
+    <Wrapper>
       <RouterProvider router={router} />
     </Wrapper>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/data/menu-data.ts
+++ src/data/menu-data.ts
@@ -1,14 +1,13 @@
-
 const menu_data = [
   {
     id: 1,
     title: "Home",
-    link: "#",
+    link: "/",
     has_dropdown: true,
     sub_menus: [
-      { link: "/", title: "Home 01", },
-      { link: "/home-2", title: "Home 02", },
-    ]
+      { link: "/", title: "Home 01" },
+      { link: "/home-2", title: "Home 02" },
+    ],
   },
   {
     id: 2,
@@ -28,45 +27,43 @@ const menu_data = [
         inner_submenu: [
           { link: "/service", title: "Service" },
           { link: "/service-details", title: "Service Details" },
-
-        ]
+        ],
       },
       {
         link: "#",
         title: "Authentification",
         has_inner_dropdown: true,
         inner_submenu: [
-          { link: "/register", title: "Register" }, 
-          { link: "/login", title: "Login" }, 
-
-        ]
+          { link: "/register", title: "Register" },
+          { link: "/login", title: "Login" },
+        ],
       },
       {
         link: "/pricing-plan",
         title: "Pricing Plan",
-        has_inner_dropdown: false, 
+        has_inner_dropdown: false,
       },
       {
         link: "/faq",
         title: "FAQ",
-        has_inner_dropdown: false, 
+        has_inner_dropdown: false,
       },
       {
         link: "/team",
         title: "Team",
-        has_inner_dropdown: false, 
+        has_inner_dropdown: false,
       },
       {
         link: "/team-details",
         title: "Team Details",
-        has_inner_dropdown: false, 
+        has_inner_dropdown: false,
       },
       {
         link: "/404",
         title: "404 - Error",
-        has_inner_dropdown: false, 
+        has_inner_dropdown: false,
       },
-    ]
+    ],
   },
   {
     id: 3,
@@ -74,9 +71,9 @@ const menu_data = [
     link: "#",
     has_dropdown: true,
     sub_menus: [
-      { link: "/portfolio", title: "Portfolio", }, 
-      { link: "/portfolio-details", title: "Portfolio Details", }, 
-    ]
+      { link: "/portfolio", title: "Portfolio" },
+      { link: "/portfolio-details", title: "Portfolio Details" },
+    ],
   },
   {
     id: 4,
@@ -84,11 +81,10 @@ const menu_data = [
     link: "#",
     has_dropdown: true,
     sub_menus: [
-      { link: "/blog", title: "Blog", }, 
-      { link: "/blog-details", title: "Blog Details", }, 
-    ]
+      { link: "/blog", title: "Blog" },
+      { link: "/blog-details", title: "Blog Details" },
+    ],
   },
+];
 
-]
-
-export default menu_data
+export default menu_data;
