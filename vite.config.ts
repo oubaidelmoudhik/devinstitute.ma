fa5a79fe304a8cb4338f7d4cd77148ac58bcb459
oubaidelmoudhik <oubaidm@live.com>
2025-04-18 21:01:48 +0100

    added image opti plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      mozjpeg: { quality: 75 },
+      optipng: { optimizationLevel: 5 },
+      webp: { quality: 80 },
+    }),
+  ],
   build: {
     outDir: "dist",
   },
