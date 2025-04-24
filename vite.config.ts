a4b46784bc512a2e1d210e081a60d6f759b0dc26
oubaidelmoudhik <oubaidm@live.com>
2025-04-24 19:57:57 +0100

    added esbuild to minify ts code, and added a small vite plugin to remove comments from html on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, Plugin, IndexHtmlTransformContext } from "vite";
 import react from "@vitejs/plugin-react";
 
+const removeHtmlCommentsPlugin = (): Plugin => {
+  return {
+    name: "remove-html-comments",
+    apply: "build",
+    transformIndexHtml(html: string, _ctx: IndexHtmlTransformContext): string {
+      return html.replace(/<!--[\s\S]*?-->/g, "");
+    },
+  };
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeHtmlCommentsPlugin()],
   build: {
     outDir: "dist",
+    minify: "esbuild",
   },
+  esbuild: { legalComments: "none" },
   server: {
     open: true,
   },
