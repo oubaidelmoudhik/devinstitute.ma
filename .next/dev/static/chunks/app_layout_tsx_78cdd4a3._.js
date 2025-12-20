45d9e089d7432274cb5e687f7e5befc7fa5b35d1
oubaidelmoudhik <oubaidm@live.com>
2025-12-20 21:40:06 +0100

    🌐 feat: complete i18n system overhaul and responsive design improvements - Refactor all translation keys to sectioned format (cta_1, services_1, etc.) - Add off-canvas component translations - Fix footer translation issues - Implement responsive font sizing for hero title - Update all components to use new translation keys - Add new offcanvas namespace to i18n system

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++ .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
@@ -2,8 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/_21fd2206._.css",
-  "static/chunks/node_modules_96ae3170._.js",
-  "static/chunks/_5f13f05d._.js",
+  "static/chunks/node_modules_ec02fc08._.js",
+  "static/chunks/_7d125cf1._.js",
   "static/chunks/node_modules_3d000988._.js"
 ],
     source: "dynamic"
