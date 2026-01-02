3fa7ad7c7a0b0fd66ec406e7afdb6c83e554e073
oubaidelmoudhik <oubaidm@live.com>
2026-01-02 15:40:42 +0100

    fix: sitemap base url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://devinstitute.ma'
+  const baseUrl = 'https://devagency.ma'
 
   return [
     {
